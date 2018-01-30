6c128f97be7a675f96007d53b39fd8e747924413
Godwhoa <gjdaniel1999@gmail.com>
2018-01-31 02:04:48 +0530

    Pass the logger

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"github.com/godwhoa/oodle/bot"
 	"github.com/godwhoa/oodle/plugins"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	oodle_bot := bot.NewBot()
+	logger := logrus.New()
+	logger.Formatter = &logrus.TextFormatter{DisableColors: true}
+	oodle_bot := bot.NewBot(logger)
 	plugins.RegisterEcho("oodle-dev", oodle_bot)
-	oodle_bot.Run(bot.Config{
+	err := oodle_bot.Run(bot.Config{
 		Nick:    "oodle-dev",
 		Server:  "chat.freenode.net",
 		Port:    6697,
 		Channel: "##oodle-test",
 	})
+	logger.Fatal(err)
 }
