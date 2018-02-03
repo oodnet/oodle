a2bf20d965bc0ff1c05a8178e74cb0d1f94a8fe8
Godwhoa <gjdaniel1999@gmail.com>
2018-02-03 06:12:23 +0530

    Make SendQueue public in base trigger

--- oodle/base.go
+++ oodle/base.go
@@ -2,9 +2,9 @@ package oodle
 
 // BaseTrigger is just an implementation of Trigger to reduce some boilerplate code
 type BaseTrigger struct {
-	sendqueue chan string
+	SendQueue chan string
 }
 
 func (btrigger *BaseTrigger) SetSendQueue(sendqueue chan string) {
-	btrigger.sendqueue = sendqueue
+	btrigger.SendQueue = sendqueue
 }
