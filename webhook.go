50019a1463fb78dd7932c619e50af03072b20681
Godwhoa <gjdaniel1999@gmail.com>
2018-02-15 14:05:06 +0530

    Add send endpoint

--- webhook.go
+++ webhook.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"html"
 	"net/http"
 
 	"github.com/godwhoa/oodle/bot"
@@ -24,30 +22,25 @@ func NewWebHook(irc *bot.IRCClient, log *logrus.Logger, secret string) *WebHook 
 	}
 }
 
-type Thread struct {
-	User      string `json:"user"`
-	Title     string `json:"title"`
-	Permalink string `json:"permalink"`
-}
-
-func (webhook *WebHook) NewThread(w http.ResponseWriter, r *http.Request) {
+func (webhook *WebHook) Send(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" || r.Header.Get("X-SECRET") != webhook.secret {
 		http.Error(w, "Invalid secret", 400)
 		return
 	}
-	t := &Thread{}
-	err := json.NewDecoder(r.Body).Decode(t)
+	msgs := []string{}
+	err := json.NewDecoder(r.Body).Decode(&msgs)
 	if err != nil {
 		http.Error(w, "Malformed JSON", 400)
 		return
 	}
-	webhook.irc.Send(fmt.Sprintf(`oods.net: %s started thread "%s"`, t.User, html.UnescapeString(t.Title)))
-	webhook.irc.Send(t.Permalink)
+	for _, msg := range msgs {
+		webhook.irc.Send(msg)
+	}
 	w.Write([]byte(`OK`))
 }
 
 func (webhook *WebHook) Listen(addr string) error {
-	http.HandleFunc("/event/thread", webhook.NewThread)
+	http.HandleFunc("/send", webhook.Send)
 	webhook.log.Infof("Starting webhooks on %s", addr)
 	return http.ListenAndServe(addr, nil)
 }
