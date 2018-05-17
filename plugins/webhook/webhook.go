87ca05bceabe0805cdef88366f927ab316053f72
Godwhoa <gjdaniel1999@gmail.com>
2018-05-18 03:29:24 +0530

    Make webhook a plugin

---
+++ plugins/webhook/webhook.go
@@ -0,0 +1,46 @@
+package webhook
+
+import (
+	"encoding/json"
+	"html"
+	"net/http"
+
+	"github.com/godwhoa/oodle/oodle"
+	"github.com/lrstanley/girc"
+	"github.com/sirupsen/logrus"
+)
+
+func Register(deps oodle.Deps) error {
+	wh := &webhook{irc: deps.IRC, log: deps.Logger, secret: deps.Config.Secret}
+	go wh.Listen(deps.Config.WebHookAddr)
+	return nil
+}
+
+type webhook struct {
+	irc    oodle.IRCClient
+	log    *logrus.Logger
+	secret string
+}
+
+func (wh *webhook) Send(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" || r.Header.Get("X-SECRET") != wh.secret {
+		http.Error(w, "Invalid secret", 400)
+		return
+	}
+	msgs := []string{}
+	err := json.NewDecoder(r.Body).Decode(&msgs)
+	if err != nil {
+		http.Error(w, "Malformed JSON", 400)
+		return
+	}
+	for _, msg := range msgs {
+		wh.irc.Send(girc.Fmt(html.UnescapeString(msg)))
+	}
+	w.Write([]byte(`OK`))
+}
+
+func (wh *webhook) Listen(addr string) {
+	http.HandleFunc("/send", wh.Send)
+	wh.log.Infof("Starting webhook on %s", addr)
+	wh.log.Fatalf("Webhook failed: %v", http.ListenAndServe(addr, nil))
+}
