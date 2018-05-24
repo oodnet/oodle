398de59bf5be0ff1fab4e2ff4dc0fd822704b7fd
Godwhoa <gjdaniel1999@gmail.com>
2018-05-24 10:18:54 +0530

    Add Alias

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"strings"
 	"time"
 
 	"github.com/hako/durafmt"
@@ -18,4 +19,14 @@ func FmtDur(d time.Duration) string {
 	d = d.Truncate(time.Second)
 	// formats it to 1 second etc.
 	return durafmt.Parse(d).String()
+}
+
+// Alias returns a new command with replaced prefix, name and usage
+func Alias(prefix, name string, cmd Command) Command {
+	firstFragment := strings.Split(cmd.Usage, " ")[0] // usually it's command's prefix+name
+	usage := strings.Replace(cmd.Usage, firstFragment, prefix+name, 1)
+	cmd.Prefix = prefix
+	cmd.Name = name
+	cmd.Usage = usage
+	return cmd
 }
