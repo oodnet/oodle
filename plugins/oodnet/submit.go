4cfe1c5a19a34245557ff21daf9f207f8c5f375e
Joseph Daniel <gjdaniel1999@gmail.com>
2019-04-12 22:12:58 +0530

    Add screenshot command for oodnet

--- plugins/oodnet/submit.go
+++ plugins/oodnet/submit.go
@@ -16,7 +16,7 @@ func Register(deps *oodle.Deps) error {
 	if key == "" {
 		return nil
 	}
-	bot.RegisterCommands(Submit(checker, key))
+	bot.RegisterCommands(Submit(checker, key), Screenshot(checker, key))
 	return nil
 }
 
@@ -39,6 +39,34 @@ func Submit(checker oodle.Checker, key string) oodle.Command {
 			form.Set("text", desc)
 			form.Set("password", key)
 			_, err := u.HTTPClient.PostForm("https://oods.net/submit.php", form)
+			if err != nil {
+				return "Failed to submit: " + err.Error(), nil
+			}
+			return "Submitted!", nil
+		},
+	}
+	return m.Chain(cmd, m.MinArg(2), m.RegisteredOnly(checker))
+}
+
+func Screenshot(checker oodle.Checker, key string) oodle.Command {
+	cmd := oodle.Command{
+		Prefix:      ".",
+		Name:        "screenshot",
+		Description: "IDK, ask exezin.",
+		Usage:       ".screenshot <desc> <url>",
+		Fn: func(nick string, args []string) (string, error) {
+			desc := strings.Join(args[:len(args)-1], " ")
+			rawurl := args[len(args)-1]
+
+			if _, err := url.ParseRequestURI(rawurl); err != nil {
+				return "Last arg. isn't an url", nil
+			}
+			form := url.Values{}
+			form.Set("url", rawurl)
+			form.Set("username", nick)
+			form.Set("text", desc)
+			form.Set("password", key)
+			_, err := u.HTTPClient.PostForm("https://oods.net/screenshot-submit", form)
 			if err != nil {
 				return "Failed to submit: " + err.Error(), nil
 			}
