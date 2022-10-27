e9cf0b7b93b4f6408d89fa8de918d133bd3c5cc9
solenum <young96steven@gmail.com>
2022-10-27 13:33:52 +0100

    new webhook for discord to irc bridge

--- plugins/webhook/webhook.go
+++ plugins/webhook/webhook.go
@@ -35,6 +35,10 @@ type webhook struct {
 	watch map[string][]string
 }
 
+type DiscordMessage struct {
+	Content string `json:"content"`
+}
+
 func (wh *webhook) Send(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" || r.Header.Get("X-SECRET") != wh.secret {
 		http.Error(w, "Invalid secret", 400)
@@ -52,12 +56,28 @@ func (wh *webhook) Send(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`OK`))
 }
 
+func (wh *webhook) DiscordRelay(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" || r.Header.Get("X-SECRET") != wh.secret {
+		http.Error(w, "Invalid secret", 400)
+		return
+	}
+	var message DiscordMessage
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		http.Error(w, "Malformed JSON", 400)
+		return
+	}
+	wh.irc.Send(girc.Fmt(html.UnescapeString(message.Content)))
+	w.Write([]byte(`OK`))
+}
+
 func (wh *webhook) Listen(addr string) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`oodle!`))
 	})
 	http.HandleFunc("/send", wh.Send)
 	http.HandleFunc("/github", wh.Github)
+	http.HandleFunc("/relay", wh.DiscordRelay)
 	wh.log.Infof("Starting webhook on %s", addr)
 	wh.log.Fatalf("Webhook failed: %v", http.ListenAndServe(addr, nil))
 }
